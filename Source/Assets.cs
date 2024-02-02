6ae7a2836bf02416b937b26522a09c90ea2d70ce
ICE AGE <465578756@qq.com>
2024-02-02 22:07:22 +0800

    Update Assets.cs
    添加任意字符支持

--- Source/Assets.cs
+++ Source/Assets.cs
@@ -122,16 +122,46 @@ public static class Assets
 		// load audio
 		Audio.Load(Path.Join(ContentPath, "Audio"));
 
+		List<int> list = new List<int>();
+		for (int i = 32; i < 128; i++)
+		{
+			list.Add(i);
+		}
+		Dictionary<int, int> temp = new Dictionary<int, int>();
 		// load level json
 		{
 			var data = File.ReadAllText(Path.Join(ContentPath, "Levels.json"));
 			Levels = JsonSerializer.Deserialize(data, LevelInfoListContext.Default.ListLevelInfo) ?? [];
+			foreach (var item in Levels)
+			{
+				foreach (var chr in item.Name)
+				{
+					if (!temp.ContainsKey(chr))
+						temp.Add(chr, chr);
+				}
+				foreach (var chr in item.Label)
+				{
+					if (!temp.ContainsKey(chr))
+						temp.Add(chr, chr);
+				}
+			}
 		}
 
 		// load dialog json
 		{
 			var data = File.ReadAllText(Path.Join(ContentPath, "Dialog.json"));
 			Dialog = JsonSerializer.Deserialize(data, DialogLineDictContext.Default.DictionaryStringListDialogLine) ?? [];
+			foreach (var item in Dialog)
+			{
+				foreach (var line in item.Value)
+				{
+					foreach (var chr in line.Text)
+					{
+						if (!temp.ContainsKey(chr))
+							temp.Add(chr, chr);
+					}
+				}
+			}
 		}
 
 		// load glsl shaders
@@ -144,12 +174,12 @@ public static class Assets
 				Shaders[shader.Name] = shader;
 			}
 		}
-
+		var char2 = temp.Values.ToArray();
 		// load font files
 		var fontsPath = Path.Join(ContentPath, "Fonts");
 		foreach (var file in Directory.EnumerateFiles(fontsPath, "*.*", SearchOption.AllDirectories))
 			if (file.EndsWith(".ttf") || file.EndsWith(".otf"))
-				Fonts.Add(GetResourceName(fontsPath, file), new SpriteFont(file, FontSize));
+				Fonts.Add(GetResourceName(fontsPath, file), new SpriteFont(file, FontSize, char2));
 
 		// pack sprites into single texture
 		{
@@ -241,4 +271,4 @@ public static class Assets
 
 [JsonSourceGenerationOptions(WriteIndented = true)]
 [JsonSerializable(typeof(Dictionary<string, List<Assets.DialogLine>>))]
-internal partial class DialogLineDictContext : JsonSerializerContext {}
+internal partial class DialogLineDictContext : JsonSerializerContext { }
